56cc843c590b7ab10fe126c47b33aea6cf226c5e
nsungilwa <sungiboymike@gmail.com>
2025-06-24 13:20:46 +0100

    revervation . commande php

---
+++ reservation.php
@@ -0,0 +1,25 @@
+<?php
+    //Récupération des élements du formulaire
+    $noms = $_POST['nom'];
+    $dates = $_POST['dates'];
+    $heure = $_POST['heure'];
+    $nombredepersonnes = $_POST['personnes'];
+    
+
+    try{
+        $bdd = new PDO("mysql:host=localhost;dbname=vente", "root", "");
+        echo "Connexion reussie";
+    }
+    catch(Exception $e){
+        die("Erreur : " .$e->getMessage());
+    }
+
+    //Requête pour inserer les données
+    $req = $bdd->prepare("INSERT INTO reservation (noms, dates, heure, nombredepersonnes) VALUES(:noms, :dates, :heure, :personnes)");
+    $req->bindParam(':noms', $noms);
+    $req->bindParam(':dates', $dates);
+    $req->bindParam(':heure', $heure);
+    $req->bindParam(':nombredepersonnes', $personnes);
+    
+    $req->execute();
+?>

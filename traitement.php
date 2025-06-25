aad81db55fa918f3b85851789ce79279fb79a345
nsungilwa <sungiboymike@gmail.com>
2025-06-25 14:31:25 +0100

    Remplacement de php

--- traitement.php
+++ traitement.php
@@ -1,26 +1,27 @@
-
 <?php
-    //Récupération des élements du formulaire
-    $noms = $_POST['nom'];
-    $telephone = $_POST['telephone'];
-    $plat_commander = $_POST['plat'];
-    $quantite = $_POST['quantite'];
-    $adresse = $_POST['adresse'];
+// Récupération des éléments du formulaire
+$noms = $_POST['nom'];
+$telephone = $_POST['telephone'];
+$plat_commander = $_POST['plat'];
+$quantite = $_POST['quantite'];
+$adresse = $_POST['adresse'];
 
-    try{
-        $bdd = new PDO("mysql:host=localhost;dbname=vente", "root", "");
-        echo "Connexion reussie";
-    }
-    catch(Exception $e){
-        die("Erreur : " .$e->getMessage());
-    }
+try {
+    $bdd = new PDO("mysql:host=localhost;dbname=vente", "root", "");
+    echo "Connexion reussie";
+} catch (Exception $e) {
+    die("Erreur : " . $e->getMessage());
+}
 
-    //Requête pour inserer les données
-    $req = $bdd->prepare("INSERT INTO commander(noms, telephone, platcommande, quantite, adresse) VALUES(:noms, :telephone, :platcommande, :quantite, :adresse)");
-    $req->bindParam(':noms', $noms);
-    $req->bindParam(':telephone', $telephone);
-    $req->bindParam(':platcommande', $plat_commander);
-    $req->bindParam(':quantite', $quantite);
-    $req->bindParam(':adresse', $adresse);
-    $req->execute();
+// Préparation et exécution de la requête
+$req = $bdd->prepare("INSERT INTO commander(noms, telephone, platcommande, quantite, adresse)
+                      VALUES(:noms, :telephone, :platcommande, :quantite, :adresse)");
+
+$req->bindParam(':noms', $noms);
+$req->bindParam(':telephone', $telephone);
+$req->bindParam(':platcommande', $plat_commander);
+$req->bindParam(':quantite', $quantite);
+$req->bindParam(':adresse', $adresse);
+
+$req->execute();
 ?>
